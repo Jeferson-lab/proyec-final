85bebd8c92ed7c251bf4da911aa6b6d8d30a782d
Jeferson-lab <salazarcustodiojeferson739@gmail.com>
2024-12-04 13:15:23 -0500

    wewe

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  test: {
+    globals: true, // Habilita `describe`, `it`, etc. globalmente
+    environment: 'jsdom', // Entorno DOM simulado
+    setupFiles: './src/test/setup.ts', // Archivo de configuración global
   },
 });
